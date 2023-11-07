0a9de750512d42bc7dacf482d5f12473d59b22c6
RafiRusyaidi <143974288+RafiRusyaidi@users.noreply.github.com>
2023-11-07 12:14:35 +0800

    update

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,7 +1,24 @@
 import os
 
 
+def user_input(input1, input2):
+    # rename
+    old_file_name = f"../pythonProject/folder/{input1}.txt"
+    new_file_name = f"../pythonProject/folder/{input2}.txt"
+    os.rename(old_file_name, new_file_name)
+
+
 def folder():
+    # list file
+    folder_path = "C:/Users/rusya/Desktop/DFP40203_MK10_Selasa/pythonProject/folder"
+    files = os.listdir(folder_path)
+    for file in files:
+        print(file)
+
+    oldname = input("Enter a old: ")
+    newname = input("Enter a new: ")
+    user_input(oldname, newname)
+
     folder_path = "C:/Users/rusya/Desktop/DFP40203_MK10_Selasa/pythonProject/folder"
     files = os.listdir(folder_path)
     for file in files:
